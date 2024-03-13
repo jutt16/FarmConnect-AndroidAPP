4c72ab6a9275beb2cd567c3a9d84eac60c0b3862
jutt16 <shahidfaqeer02@gmail.com>
2024-03-13 16:20:55 +0500

    Developing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
